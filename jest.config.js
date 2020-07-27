b1d1bcc4f4457236c341bfe2a66844e79fb6468d
msk- <mattkingston@gmail.com>
2020-07-27 17:48:43 +0100

    Added reporter

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
-
 module.exports = {
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                filename: 'results_api.html',
+            },
+        ],
+        [
+            '../../lib/json-jest-reporter/index.js',
+            {
+                outputFile: 'results_api.json',
+            },
+        ],
+    ],
     testEnvironment: "node",
     testTimeout: 15000,
 };
